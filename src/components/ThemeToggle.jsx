ae2bddc0e6fc8b2032fda23fb02f656a8683d1f6
Taraka Saikumar <67173900+TarakaSaiKumar@users.noreply.github.com>
2025-08-01 01:41:43 +0530

    ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -25,5 +25,5 @@ export const ThemeToggle = () => {
             setisDarkmode(true)
         }
     }
-    return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300")}> {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-900" />}</button>
+    return <button onClick={toggleTheme} className={cn("fixed max-sm:pr-20 top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300")}> {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-900" />}</button>
 }
